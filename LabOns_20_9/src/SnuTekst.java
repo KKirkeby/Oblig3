6354c17211d27400a093611c78df4a639b965cdb
KristianKirkeby <kristian.kirkeby@hotmail.com>
2015-03-04 03:28:58 +0100

    Commit versjon 0.1

---
+++ LabOns_20_9/src/SnuTekst.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class SnuTekst {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Skriv inn et orrd til reversering: ");
+		String str = input.nextLine();
+		System.out.println();
+		baklengs(str);
+	}
+		
+	    public static void baklengs (String tekst){
+	        if(tekst.length() ==0)
+	        	return;
+	
+	        System.out.println("Strengen skrevet ut bakleng: "+ (tekst.length() - 1));
+	        baklengs(tekst.substring(0, tekst.length() - 1));
+}}
+
